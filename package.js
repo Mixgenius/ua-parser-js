0bac38a6714b9cdc5341ad6e05c2a126c8964ebe
Henry Myers <hmyers@landr.com>
2017-08-16 15:52:24 -0400

    Update package.json

--- package.js
+++ package.js
@@ -1,8 +1,12 @@
 Package.describe({
-  name: 'faisalman:ua-parser-js',
+  name: 'ua-parser-js-amplitude',
+  author: 'Faisal Salman <f@faisalman.com> (http://faisalman.com)',
+  contributors: [
+    'Daniel Jih'
+  ],
   version: '0.7.10',
-  summary: 'Lightweight JavaScript-based user-agent string parser',
-  git: 'https://github.com/faisalman/ua-parser-js.git',
+  summary: 'A fork of Amplitude\'s fork of faisalman\'s lightweight JavaScript-based user-agent string parser',
+  git: 'https://github.com/Mixgenius/ua-parser-js.git',
   documentation: 'readme.md'
 });
 
